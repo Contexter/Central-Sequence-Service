6defd1f0dcd2ca4e8008c2c08bebc5a2b20abd04
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-29 04:44:01 +0100

    Update .gitignore and remove accidentally added build folder

--- .build/arm64-apple-macosx/debug/CNIOBoringSSL.build/gen/bcm/rdrand-x86_64-linux.S.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/module.modulemap \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/gen/bcm/rdrand-x86_64-linux.S \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_asm_base.h \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_target.h \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_boringssl_prefix_symbols_asm.h
--- .build/arm64-apple-macosx/debug/CNIOBoringSSL.build/gen/bcm/sha512-586-apple.S.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/module.modulemap \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/gen/bcm/sha512-586-apple.S \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_asm_base.h \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_target.h \
-  /Users/benedikteickhoff/Development/Github-Desktop/Central-Sequence-Service/.build/checkouts/swift-nio-ssl/Sources/CNIOBoringSSL/include/CNIOBoringSSL_boringssl_prefix_symbols_asm.h
