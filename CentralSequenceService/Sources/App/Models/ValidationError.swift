29f5b9e5c5d49be700cc75f221898a6c605b50c7
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-12-06 03:19:57 +0100

    Clean slate for Vapor new setup

--- CentralSequenceService/Sources/App/Models/ValidationError.swift
+++
@@ -1,7 +0,0 @@
-import Vapor
-
-struct ValidationError: Swift.Error, Content {
-    let reason: String
-    let suggestion: String
-    let context: String
-}
--- CentralSequenceService/Sources/App/Validators/Validator.swift
+++
@@ -1,6 +0,0 @@
-import Vapor
-import OpenAPIKit
-
-protocol Validator {
-    func validate(request: Request, operation: OpenAPI.Operation) throws
-}
