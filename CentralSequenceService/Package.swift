910e14c7b52c8206ffba768eca3e5fc40b5ca589
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-12-11 05:08:53 +0100

    run Hello World successfully

--- CentralSequenceService/Package.swift
+++ CentralSequenceService/Package.swift
@@ -1,14 +1,41 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "CentralSequenceService",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    dependencies: [
+        // OpenAPI Generator
+        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.5.0"),
+        .package(url: "https://github.com/swift-server/swift-openapi-vapor.git", from: "1.0.1"), // Updated version
+
+        // Vapor
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+
+        // Fluent and SQLite Driver
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.3.0"),
+
+        // Typesense Client
+        .package(url: "https://github.com/typesense/typesense-swift.git", from: "1.0.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "CentralSequenceService"),
+            name: "CentralSequenceService",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Typesense", package: "typesense-swift"), // Correct product name
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ]
+        )
     ]
 )
