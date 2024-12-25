675e62e5065533f1acd17e24c73c20ce77b43b73
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-12-25 05:59:20 +0100

    Archived existing iterations and handlers to prepare for Iteration 7

--- CentralSequenceService/Sources/Iterations/iteration_1.swift
+++
@@ -1,6 +0,0 @@
-import Vapor
-
-public func iteration_1(app: Application) {
-    print("Iteration 1 logic executed.")
-    // Add your logic here
-}
--- CentralSequenceService/Sources/Iterations/iteration_2.swift
+++
@@ -1,6 +0,0 @@
-import Vapor
-
-public func iteration_2(app: Application) {
-    print("Iteration 2 logic executed.")
-    // Add logic for Iteration 2 here
-}
